9a3732a2ba39f840e21d34bfebcd8a819f0558ca
vishist.br <vishist.br@rakuten.com>
2023-06-14 18:08:17 +0530

    portfolio-commit-2

---
+++ SideProjects/portfolio/v-portfolio/app/page.tsx
@@ -0,0 +1,49 @@
+"use client"
+import About from './components/About'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import WorkExperience from './components/WorkExperience'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import ContactMe from './components/ContactMe'
+
+
+export default function Home() {
+  return (
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
+
+    {/* Header */}
+    <Header />
+    
+    {/* hero */}
+    <section id="hero" className="snap-start" >
+      <Hero />
+    </section>
+
+    {/* About */}
+    <section id="About" className="snap-center" >
+      <About />
+    </section>
+
+    {/* Experience */}
+    <section id="experience" className="snap-center" >
+      <WorkExperience />
+    </section>
+
+    {/* Skills */}
+    <section id="skills" className="snap-center" >
+      <Skills />
+    </section>
+
+    {/* Projects */}
+    <section id="projects" className="snap-center" >
+      <Projects />
+    </section>
+
+    {/* Contact Me */}
+    <section id="contact" className="snap-center" >
+      <ContactMe />
+    </section>
+    </div>
+  )
+}
